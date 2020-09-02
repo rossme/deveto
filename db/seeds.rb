8863a6bf6f38d992ecb7b1e780354b64280f31ce
Ross Buddie <r.buddie@gmail.com>
2020-09-02 16:22:06 +0100

    Netflix API etc

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+### For API movie seeds refers to parsing method in movie.rb
+require 'uri'
+require 'net/http'
+require 'openssl'
+
+movies = NetflixApiService.parsing
+
+#DB movie schema needs changing to match API schema.
+movies.each do |movie|
+  Movie.create!(
+    title: movie['title'],
+  )
+end
