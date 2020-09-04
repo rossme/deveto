d654c307e856d89d96d188c4f72a58d3242e5b13
Meg Bailey <megxbailey@gmail.com>
2020-09-04 15:43:09 +0100

    this countdown does it's job better, not like that stupid countdown

--- app/javascript/components/randomize.js
+++ app/javascript/components/randomize.js
@@ -1,15 +1,16 @@
 const randomizeMovieNameOnClick = () => {
-
-const movies = document.querySelector("#randomize").dataset.movies.split(",");
-const moviename = document.querySelector("#randomize");
-const countdown = document.querySelector("#countdown");
-Array.prototype.random = function () {
+  const movies = document.querySelector("#randomize").dataset.movies.split(",");
+  const moviename = document.querySelector("#randomize");
+  const countdown = document.querySelector("#countdown");
+  Array.prototype.random = function () {
     return this[Math.floor((Math.random()*this.length))];
   }
+  
+  let btimer;
 
   function timer(seconds, cb) {
     var remaningTime = seconds;
-    window.setTimeout(function() {
+    btimer = window.setTimeout(function() {
       cb();
       console.log(remaningTime);
       if (remaningTime > 0) {
@@ -22,23 +23,20 @@ Array.prototype.random = function () {
       }
     }, 1000);
   }
-  
+
   var callback = function() {
-    clearTimeout(timer)
+    clearTimeout(btimer)
   };
   
- 
-
-document.querySelector("#randomize-button").addEventListener('click', function()
-{
- document.querySelector("#randomize").innerHTML = "";
- let movie = movies.random().replace(/("|"$)/g, '');
- let movierandom = document.createElement("H1");
- movierandom.innerHTML = movie;
- moviename.prepend(movierandom);
- window.clearTimeout(timer);
- timer(30, callback); 
-})
+  document.querySelector("#randomize-button").addEventListener('click', function() {
+    document.querySelector("#randomize").innerHTML = "";
+    let movie = movies.random().replace(/("|"$)/g, '');
+    let movierandom = document.createElement("H1");
+    movierandom.innerHTML = movie;
+    moviename.prepend(movierandom);
+    window.clearTimeout(btimer);
+    timer(30, callback); 
+  })
 };
 
 export { randomizeMovieNameOnClick }
