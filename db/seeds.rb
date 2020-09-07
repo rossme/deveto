ec42811217e98820cbd0d740c1ad8f8c300296a9
Ross Buddie <r.buddie@gmail.com>
2020-09-07 12:22:10 +0100

    seed file master

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,113 +8,33 @@ puts "DESTROYING USERS..."
 puts "DESTROYING HOUSEHOLDS..."
 puts "DESTROYING USER HOUSEHOLDS..."
 puts "DESTROYING DANNY..."
-
 UserHousehold.destroy_all
 Household.destroy_all
 User.destroy_all
-
+ user = User.create!({
+  name: "Danny",
+  email: "danny@deve.to",
+  password: "123456",
+ })
+ user.save
 puts "SEEDING USERS..."
-
-data_users = [
-  {
-    name: "Ricardo",
-    email: "ricardo@gmail.com",
-    password: "123456"
-  },
-  {
-    name: "Cass",
-    email: "cass@gmail.com",
-    password: "123456"
-  },
-  {
-    name: "Megan",
-    email: "megan@gmail.com",
-    password: "123456"
-  },
-  {
-    name: "Lara",
-    email: "lara@gmail.com",
-    password: "123456"
-  },
-  {
-    name: "Eukleyv",
-    email: "eukleyv@gmail.com",
-    password: "123456"
-  },
-  {
-    name: "Branca",
-    email: "branca@gmail.com",
-    password: "123456"
-  },
-  {
-    name: "Samira",
-    email: "samira@gmail.com",
-    password: "123456"
-
-  },
-  {
-    name: "Maria",
-    email: "maria@gmail.com",
-    password: "123456"
-
-  },
-  {
-    name: "Louis",
-    email: "louis@gmail.com",
-    password: "123456"
-  },
-  {
-    name: "Adriano",
-    email: "adriano@gmail.com",
-    password: "123456"
-  },
-  {
-    name: "Danny",
-    email: "danny@deve.to",
-    password: "123456"
-  }
-]
-
-data_users.each do |props|
-  user = User.create!({
-  name: props[:name],
-  email: props[:email],
-  password: props[:password]
-  })
+20.times do
+ user = User.create!({
+  name: Faker::Name.first_name,
+  email: Faker::Internet.email,
+  password: Faker::Internet.password,
+ })
+ user.save
 end
-
-
 puts "SEEDING HOUSEHOLDS..."
 puts "SEEDING USERHOUSEHOLDS..."
-
-data_households = [
-  {
-    name: "Family",
-  },
-  {
-    name: "Le Wagon Friends",
-  },
-  {
-    name: "Cousin's Place",
-  },
-  {
-    name: "Friday Night Friends",
-  },
-  {
-    name: "Hodor's House",
-  }
-]
-
-data_households.each do |props|
+5.times do
   household = Household.create!({
-  name: props[:name],
-  })
-end
-
-
-
+  name: Faker::Color.unique.color_name,
+  user: user
+ })
+ household.save
 2.times do |count2|
-
   userhousehold = UserHousehold.create!({
   user: User.all[count2],
   household: household,
@@ -123,12 +43,10 @@ end
   user_turn: false,
  })
  end
-
+end
 puts "DESTROYING NETFLIX MOVIES..."
 Movie.destroy_all
-
 movies = NetflixApiService.parsing
-
 movies.each do |movie|
   Movie.create!(
     title: movie['title'],
@@ -141,4 +59,3 @@ movies.each do |movie|
     runtime: movie['runtime']
   )
 end
-
