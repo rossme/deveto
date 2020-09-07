9052cddec4975f3ffb7b6e891ebfaf9ab4e97d5a
Ross Buddie <r.buddie@gmail.com>
2020-09-07 18:09:26 +0100

    Added timer to Watch Now card - with option if it's not your turn

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,11 +32,16 @@ import { initChatroomCable } from '../channels/chatroom_channel';
 // import { initSelect2 } from '../components/init_select2';
 import { randomizeMovieNameOnClick } from "../components/randomize";
 
+import { vetoTimer } from "../components/timer";
+
+
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   //import chatroom
+
+  vetoTimer();
   initChatroomCable();
   randomizeMovieNameOnClick();
 });
